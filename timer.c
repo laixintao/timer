203e83bbae44001aabed8f4da0631cfc4c867b83
laixintao <laixintaoo@gmail.com>
2021-07-06 23:58:05 +0800

    format with cland

--- timer.c
+++ timer.c
@@ -2,22 +2,22 @@
 #include <time.h>
 #include <unistd.h>
 
-void print_current_time(){
-    time_t current_time;
-    char c_time_string[300];
-    current_time = time(NULL);
+void print_current_time() {
+  time_t current_time;
+  char c_time_string[300];
+  current_time = time(NULL);
 
-    struct tm *p = localtime(&current_time);
-    strftime(c_time_string, 300, "%Y-%b-%d %X", p);
+  struct tm *p = localtime(&current_time);
+  strftime(c_time_string, 300, "%Y-%b-%d %X", p);
 
-    printf("\rcurrent time: %s", c_time_string);
-    fflush(stdout);
+  printf("\rcurrent time: %s", c_time_string);
+  fflush(stdout);
 }
 
-int main(void){
-    while (1){
-        print_current_time();
-        sleep(1);
-    }
-    return 0;
+int main(void) {
+  while (1) {
+    print_current_time();
+    sleep(1);
+  }
+  return 0;
 }
